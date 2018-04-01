158cb1252c4ef7cc4ccdf8d816e38dc0d7c045aa
Gheorghe Popescu <gheorghe.popescu@unifiedpost.com>
2018-04-01 20:50:52 +0300

    Keep all statuses

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -8,14 +8,14 @@ redis_config = {
 Sidekiq.configure_client do |config|
   config.redis = redis_config
   # accepts :expiration (optional)
-  Sidekiq::Status.configure_client_middleware config, expiration: 30.minutes
+  Sidekiq::Status.configure_client_middleware config, expiration: 30.days
 end
 
 Sidekiq.configure_server do |config|
   config.redis = redis_config
   # accepts :expiration (optional)
-  Sidekiq::Status.configure_server_middleware config, expiration: 30.minutes
+  Sidekiq::Status.configure_server_middleware config, expiration: 30.days
 
   # accepts :expiration (optional)
-  Sidekiq::Status.configure_client_middleware config, expiration: 30.minutes
+  Sidekiq::Status.configure_client_middleware config, expiration: 30.days
 end
